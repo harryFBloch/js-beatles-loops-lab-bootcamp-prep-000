fb7857ef7a22721fd9a60ec1f614380593926f81
Harry Bloch <harryfbloch@gmail.com>
2018-08-28 23:02:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,3 @@ function johnLennonFacts(facts){
   return facts;
 }
 
-function iLoveTheBeatles(num){
-  var array = [];
-  do {
-    array.push("I love the Beatles!");
-    num++;
-  }while(num < 15)
-  return array;
-}
